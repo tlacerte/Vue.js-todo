e20a333bc24a573761bcc51b4a03050807058c38
Taylor Lacerte <taylormlacerte@gmail.com>
2019-11-07 13:37:18 -0600

    display sample todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,9 @@
         class="todo-input" 
         placeholder="what needs to be done"
         v-model="newTodo">
-        Todo list goes here
+        <div v-for="todo in todos" :key="todo.id" class="todo-item">
+            {{ todo.title }}
+        </div>
     </div>
 </template>
 
@@ -13,7 +15,19 @@ export default {
   name: 'todo-list',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newTodo: '',
+      todos: [
+        {
+            'id': 1,
+            'title': 'Sample todo 1',
+            'completed': false,
+        },
+        {
+            'id': 2,
+            'title': 'Sample todo 2',
+            'completed': false,
+        },
+      ]
     }
   }
 }
