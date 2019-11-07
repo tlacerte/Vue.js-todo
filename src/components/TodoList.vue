e51bf35beb25f71e44b90a74628eb7ad32629c86
Taylor Lacerte <taylormlacerte@gmail.com>
2019-11-07 13:54:27 -0600

    add a todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,7 +3,8 @@
         <input type="text" 
         class="todo-input" 
         placeholder="what needs to be done"
-        v-model="newTodo">
+        v-model="newTodo"
+        @keyup.enter="addTodo">
         <div v-for="todo in todos" :key="todo.id" class="todo-item">
             {{ todo.title }}
         </div>
@@ -16,6 +17,7 @@ export default {
   data () {
     return {
       newTodo: '',
+      idForTodo: 3,
       todos: [
         {
             'id': 1,
@@ -29,6 +31,22 @@ export default {
         },
       ]
     }
+  },
+  methods: {
+      addTodo() {
+        if (this.newTodo.trim().length == 0) {
+            return
+        }
+
+        this.todos.push({
+            id: this.idForTodo,
+            title: this.newTodo,
+            completed: false,
+        })
+
+        this.newTodo = ''
+        this.idForTodo++
+      }
   }
 }
 </script>
